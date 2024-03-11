f0a06dc9ffa35c541000a85890947eac8b11ca1a
arruejulene <arrue.julene@correo.unimet.edu.ve>
2024-03-11 12:14:51 -0400

    Initial Project Commit

---
+++ hotel-booking/src/javaapplication20/JavaApplication20.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication20;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GUI interfaz1 = new GUI();
+        interfaz1.setVisible(true);
+    }
+    
+}
