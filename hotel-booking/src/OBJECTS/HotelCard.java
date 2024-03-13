84926dde8e4366b41f88cdfc8b52dee378b7bb22
Cristiandresgp <cristian.gouveia@correo.unimet.edu.ve>
2024-03-12 20:59:30 -0400

    Objetos listos

--- hotel-booking/src/OBJECTS/HotelCard.java
+++ hotel-booking/src/OBJECTS/HotelCard.java
@@ -9,5 +9,51 @@ package OBJECTS;
  * @author cristiandresgp
  */
 public class HotelCard {
+    private int numHabitacion;
+    private Boolean entrada;
+    private Boolean salida;
+    private int estado;
+
+    public HotelCard(int numHabitacion, int estado) {
+        this.numHabitacion = numHabitacion;
+        this.entrada = false;
+        this.salida = false;
+        this.estado = estado;
+    }
+
+    public int getNumHabitacion() {
+        return numHabitacion;
+    }
+
+    public void setNumHabitacion(int numHabitacion) {
+        this.numHabitacion = numHabitacion;
+    }
+
+    public Boolean getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(Boolean entrada) {
+        this.entrada = entrada;
+    }
+
+    public Boolean getSalida() {
+        return salida;
+    }
+
+    public void setSalida(Boolean salida) {
+        this.salida = salida;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+    
+    
+    
     
 }
