e49c4cf77cceefa90d976127b979c196192aeb13
soficrisl <sleon@correo.unimet.edu.ve>
2024-03-13 20:12:20 -0400

    Data structures done

---
+++ hotel-booking/src/EDD/Bucket.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EDD;
+
+/**
+ *
+ * @author Sofia
+ */
+/*
+public class Bucket {
+    private int key;
+    
+
+    public Bucket(str key, Object element) {
+        this.key = key;
+        this.element = element;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public Object getElement() {
+        return element;
+    }
+
+    public void setElement(Object element) {
+        this.element = element;
+    }
+      
+}
+*/
---
+++ hotel-booking/src/EDD/Hastable.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EDD;
+
+/**
+ *
+ * @author Sofia
+ */
+public class Hastable {
+    private ListaDoble[] hastable; 
+    
+    public Hastable() {
+        this.hastable = new ListaDoble[97];
+        for (int i = 0; i < this.hastable.length; i++) {
+            this.hastable[i] = new ListaDoble(); 
+        }
+ 
+    }
+    public ListaDoble[] getHastable() {
+        return hastable;
+    }
+
+    public void setHastable(ListaDoble[] hastable) {
+        this.hastable = hastable;
+    }
+    public int getHashSize(){
+        return this.hastable.length;
+    }
+    
+    //Aplication of djb2 algotirthm for hash function 
+    public int hashFunction (String str){
+        long hash = 5381;
+        int c; 
+        for (int i = 0; i < str.length(); i++) {
+            c = str.charAt(i); 
+            //Multiplying by 33 done by left bitshifting, to increase speed and add to hash ascii value of current char
+            hash = ((hash << 5) + hash) + c ;
+        }
+        //Convert hash into index of array
+        int index = (int) (hash % getHashSize());  
+        return (index <0) ? index * -1: index; 
+        
+    }
+    
+    public void insert(String str, Object element) {
+        int index = hashFunction(str);
+        this.hastable[index].insertBegin(element);
+    }
+    
+    public Object delete(String str, Object element){
+        int index1 = hashFunction(str);
+        if (this.hastable[index1].getHead() != null){
+            int index2 = this.hastable[index1].searchIndex(element);
+            Object deleted = this.hastable[index1]; 
+            this.hastable[index1].deleteInIndex(index2); 
+            return deleted; 
+        }
+        return null;  
+    }
+    
+    public ListaDoble search(String str) {
+        int index1 = hashFunction(str);
+        return this.hastable[index1]; 
+    }
+    
+    public void print (){
+        for (int i = 0; i < this.hastable.length; i++) {
+            System.out.println("["+ i + "]"+ this.hastable[i].printString());
+        }
+    }
+   
+}
